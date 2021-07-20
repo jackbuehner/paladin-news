dd22a4105cb642ce5a6f1b95f56acfa6a701217e
Jack Buehner <github@jackbuehner.com>
2021-07-20 12:53:49 -0600

    add featured articles to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
+  import Featured from '../components/home/Featured.svelte';
   import ArticleCardRow from '../components/home/ArticleCardRow.svelte';
 </script>
 
 <div>
-  <ArticleCardRow label={'News'} categories={'news'} />
-  <ArticleCardRow label={'Opinions'} categories={'opinion'} />
-  <ArticleCardRow label={'Sports'} categories={'sports'} />
-  <ArticleCardRow label={'Diversity Matters'} categories={'diversity'} />
-  <ArticleCardRow label={'Arts, Campus, & Culture'} categories={'arts,campus-culture'} />
+  <Featured />
 </div>
