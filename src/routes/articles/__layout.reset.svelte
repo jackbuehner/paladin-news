648b432f4408f092e6f82c926802b25d5b0b3855
Jack Buehner <github@jackbuehner.com>
2021-07-19 20:15:13 -0600

    add compact header and use it on articles pages

---
+++ src/routes/articles/__layout.reset.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Container from '../../components/Container.svelte';
+  import CompactHeader from '../../components/header/CompactHeader.svelte';
+</script>
+
+<CompactHeader />
+
+<Container>
+  <slot />
+</Container>
