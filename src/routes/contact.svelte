b3a52a5ff90be31568784548ccb75e2ea772be3a
Jack Buehner <github@jackbuehner.com>
2021-07-20 20:39:12 -0600

    add very basic contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import PageHeading from '/src/components/PageHeading.svelte';
+</script>
+
+<PageHeading>Contact us</PageHeading>
+
+<form name="contact" data-netlify="true">
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Phone <input type="phone" name="phone" /></label>
+  </p>
+
+  <p>
+    <label
+      >Reason<select name="reason[]">
+        <option value="tip">News tips</option>
+        <option value="advertise">Advertise on <i>The Paladin</i></option>
+        <option value="contribute">Contribute an article or photo</option>
+        <option value="donate">Support <i>The Paladin</i> with a donation</option>
+        <option value="join">Join our team</option>
+        <option value="join">Other</option>
+      </select></label>
+  </p>
+  <p>
+    <label>Message<textarea name="message" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
