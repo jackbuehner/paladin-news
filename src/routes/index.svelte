92f358148b004b0dc84f3b8a4a6e37b7a3bdbcfa
Jack Buehner <github@jackbuehner.com>
2021-08-05 13:51:48 -0600

    retrive article cards in load function instead

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -64,6 +64,57 @@
   }
 </style>
 
+<script context="module" lang="ts">
+  import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load({ page, fetch }: LoadInput): Promise<LoadOutput> {
+    const hostUrl =
+      variables.mode === 'development'
+        ? 'http://localhost:3001'
+        : 'https://api.thepaladin.cristata.app';
+    const url = `${hostUrl}/api/v2/articles/public?limit=10`;
+
+    const res = {
+      news: await fetch(`${url}&category=news`),
+      opinion: await fetch(`${url}&category=opinion`),
+      sports: await fetch(`${url}&category=sports`),
+      diversity: await fetch(`${url}&category=diversity`),
+      acc: await fetch(`${url}&category=arts&category=campus-culture`),
+      featured: await fetch(`${url}&featured=true`),
+    };
+
+    if (
+      res.news.ok &&
+      res.opinion.ok &&
+      res.sports.ok &&
+      res.diversity.ok &&
+      res.acc.ok &&
+      res.featured.ok
+    ) {
+      return {
+        props: {
+          articles: {
+            news: await res.news.json(),
+            opinion: await res.opinion.json(),
+            sports: await res.sports.json(),
+            diversity: await res.diversity.json(),
+            acc: await res.acc.json(),
+            featured: await res.featured.json(),
+          },
+        },
+      };
+    }
+
+    return {
+      status: 500,
+      error: new Error(`Could not load articles for home page.`),
+    };
+  }
+</script>
+
 <script lang="ts">
   import Featured from '../components/home/Featured.svelte';
   import ArticleCardRow from '../components/home/ArticleCardRow.svelte';
@@ -72,25 +123,39 @@
   import PaladinPlusList from '/src/components/PaladinPlusList.svelte';
   import { onMount } from 'svelte';
   import { title } from '../stores/title';
+  import { variables } from '../variables';
+  import type { AggregatePaginateResult } from 'src/interfaces/aggregatePaginateResult';
+  import type { IArticle } from 'src/interfaces/articles';
 
   // set the document title
   onMount(() => ($title = undefined));
+
+  // receive the articles from the load function
+  interface Iarticles {
+    news: AggregatePaginateResult<IArticle>;
+    opinion: AggregatePaginateResult<IArticle>;
+    sports: AggregatePaginateResult<IArticle>;
+    diversity: AggregatePaginateResult<IArticle>;
+    acc: AggregatePaginateResult<IArticle>;
+    featured: AggregatePaginateResult<IArticle>;
+  }
+  export let articles: Iarticles;
 </script>
 
 <div class={'wrapper'}>
   <Container>
     <HomeHeader />
-    <Featured />
+    <Featured articles={articles.featured} />
     <div class={'sections-grid'}>
       <ArticleCardRow
         label={'News'}
-        categories={'news'}
+        articles={articles.news}
         gridArea={'news'}
         quantity={[5, 4, 2, 3]} />
       <div style={'grid-area: opinions'}>
         <ArticleCardRow
           label={'Opinions'}
-          categories={'opinion'}
+          articles={articles.opinion}
           quantity={[6, 4, 3, 3]}
           forceVertical={true} />
         <div class={'section twitter'}>
@@ -113,17 +178,17 @@
       </div>
       <ArticleCardRow
         label={'Sports'}
-        categories={'sports'}
+        articles={articles.sports}
         gridArea={'sports'}
         quantity={[3, 2, 2, 3]} />
       <ArticleCardRow
         label={'Diversity Matters'}
-        categories={'diversity'}
+        articles={articles.diversity}
         gridArea={'diversity'}
         quantity={[3, 2, 2, 3]} />
       <ArticleCardRow
         label={'Arts, Campus, & Culture'}
-        categories={'arts,campus-culture'}
+        articles={articles.acc}
         gridArea={'acc'}
         quantity={[3, 2, 2, 3]} />
       <div style={'grid-area: plus;'}>
