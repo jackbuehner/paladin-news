b8d3866ce4fbd6c7e389efffd4afdc6840482158
Jack Buehner <github@jackbuehner.com>
2021-08-05 17:43:07 -0600

    show progress bar when navigation in progress

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,15 +11,38 @@
 
 <script>
   import { title } from '../../src/stores/title';
-
   import Footer from '../components/Footer.svelte';
   import CompactHeader from '../components/header/CompactHeader.svelte';
   import Search from '/src/components/search/Search.svelte';
+  import NProgress from 'nprogress';
+  import { onMount } from 'svelte';
 
   $: title_ = $title ? `${$title} - The Paladin` : 'The Paladin';
 
   // keep track of window width
   $: windowWidth = 0;
+
+  onMount(() => {
+    // configure the navigation progress bar
+    NProgress.configure({
+      parent: 'body',
+      easing: 'ease',
+      speed: 500,
+      trickle: true,
+      trickleSpeed: 200,
+      showSpinner: false,
+    });
+
+    // show progress bar on page navigate
+    window.addEventListener('sveltekit:navigation-start', () => {
+      NProgress.start();
+    });
+
+    // hide progress bar on navigation end
+    window.addEventListener('sveltekit:navigation-end', () => {
+      NProgress.done();
+    });
+  });
 </script>
 
 <svelte:head>
