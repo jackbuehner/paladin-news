e26980b0e53670244cc0fbb45fc20f06416433e1
Jack Buehner <github@jackbuehner.com>
2022-01-04 23:46:19 +0000

    feat(insertDate): use generic type with required fields instead of IArticle interface
    This makes the insertDate type work with any document with the published_at timestamp.

--- src/utils/insertDate.ts
+++ src/utils/insertDate.ts
@@ -1,18 +1,52 @@
-import type { IArticle } from 'src/interfaces/articles';
+type InputType<T = Record<string, any>> = T & {
+  timestamps: {
+    published_at: string; // ISO date
+  };
+};
 
-function insertDate(articles: IArticle[]) {
-  return articles.map((article) => {
-    // get month, date, and year from article publish timestamp
-    if (article.timestamps && article.timestamps.published_at) {
-      const pubTimestamp = new Date(article.timestamps.published_at);
-      const date = {
+type ReturnType<T = Record<string, any>> = T & {
+  date: {
+    day: string;
+    month: string;
+    year: string;
+  };
+};
+
+/**
+ * Inserts an object with the day, month, and week from `timestamps.published_at`.
+ * Access it with `date`.
+ * 
+ * If `timestamps.published_at` is undefined, the date object will be:
+ * ```
+ * {
+ *   day: `00`,
+     month: `00`,
+     year: `0000`,
+ * }
+ * ```
+ */
+function insertDate<T>(docs: InputType<T>[]) {
+  return docs.map((doc) => {
+    // initialize a date object
+    let date = {
+      day: `00`,
+      month: `00`,
+      year: `0000`,
+    };
+
+    // get month, date, and year from document publish timestamp
+    if (doc.timestamps && doc.timestamps.published_at) {
+      const pubTimestamp = new Date(doc.timestamps.published_at);
+      // update the date object
+      date = {
         day: pubTimestamp.getUTCDate().toString().padStart(2, '0'),
         month: `${pubTimestamp.getUTCMonth() + 1}`.padStart(2, '0'), // +1 because January === 0
         year: pubTimestamp.getUTCFullYear().toString(),
       };
-      return { ...article, date };
     }
-    return article;
+
+    // return the document with the date object
+    return { ...doc, date } as ReturnType<T>;
   });
 }
 
