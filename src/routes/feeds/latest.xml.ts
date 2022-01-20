167d7c0939f8482687b64fa45d457ddaf0e04c62
Jack Buehner <github@jackbuehner.com>
2022-01-20 01:46:22 +0000

    feat(xml): add feed for latest (all) articles

---
+++ src/routes/feeds/latest.xml.ts
@@ -0,0 +1,39 @@
+import type { ServerRequest } from '@sveltejs/kit/types/hooks';
+import { variables } from '../../variables';
+import { toHTML, toXML } from '../../utils/fetchSectionXml';
+import { insertDate } from '../../utils/insertDate';
+import type { IArticle } from 'src/interfaces/articles';
+
+export async function get(
+  request: ServerRequest
+): Promise<{ headers: Record<string, string>; body: string }> {
+  const hostUrl = `${variables.SERVER_PROTOCOL}://${variables.SERVER_URL}`;
+  const url = `${hostUrl}/api/v2/articles/public?limit=20`;
+  const res = await (await fetch(url)).json();
+  const docs = insertDate<IArticle>(res.docs);
+
+  const body = toXML(
+    docs
+      .map((doc) => {
+        if (!doc.timestamps?.published_at) return null;
+        return {
+          title: doc.name,
+          description: doc.description,
+          link: `https://thepaladin.news/articles/${doc.date.year}/${doc.date.month}/${doc.date.day}/${doc.slug}`,
+          published_at: new Date(doc.timestamps.published_at),
+          html: toHTML(doc.body),
+        };
+      })
+      .filter((t) => !!t),
+    `https://thepaladin.news${request.path}`
+  );
+
+  const headers = {
+    'Cache-Control': 'max-age=0, s-maxage=3600',
+    'Content-Type': 'application/xml',
+  };
+  return {
+    headers,
+    body,
+  };
+}
