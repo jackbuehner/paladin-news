3358081bff41d28050d901c013b160fe10b31108
Jack Buehner <github@jackbuehner.com>
2022-02-06 09:47:38 +0000

    feat: make icon button styles globally available

--- src/components/IconButton.svelte
+++ src/components/IconButton.svelte
@@ -1,6 +1,7 @@
 <style>
   button,
-  a {
+  a,
+  :global(.global-icon-button) {
     display: inline-flex;
     width: 36px;
     height: 36px;
@@ -17,26 +18,31 @@
     padding: 6px;
   }
   button.isOutlined,
-  a.isOutlined {
+  a.isOutlined,
+  :global(.global-icon-button--outlined) {
     border-color: var(--border-dark);
   }
   button:hover:not(.disabled),
-  a:hover:not(.disabled) {
+  a:hover:not(.disabled),
+  :global(.global-icon-button:hover:not(.disabled)) {
     background-color: var(--button-bg-hover);
     border: 1px solid var(--button-bg-hover);
     box-shadow: var(--button-shadow-hover);
   }
   button:active:not(.disabled),
-  a:active:not(.disabled) {
+  a:active:not(.disabled),
+  :global(.global-icon-button:active:not(.disabled)) {
     background-color: var(--button-bg-active);
     border: 1px solid var(--button-bg-active);
     box-shadow: var(--button-shadow-active);
   }
-  svg {
+  svg,
+  :global(.global-icon-button svg) {
     fill: currentColor;
   }
   button.disabled,
-  a.disabled {
+  a.disabled,
+  :global(.global-icon-button.disabled) {
     color: var(--button-color-disabled);
   }
 </style>
