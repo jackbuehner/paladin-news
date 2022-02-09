399f1c35987daad2bba1ac62b458ca006b48c303
Jack Buehner <github@jackbuehner.com>
2022-02-09 23:50:24 +0000

    fix: typo and data in privacy policy banner

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -71,9 +71,9 @@
 
 <note
   ><Container
-    >We updated our <a href={'/privacy-policy'}>privacy policy</a> on Feb. 2, 2022 to reflect our new
+    >We updated our <a href={'/privacy-policy'}>privacy policy</a> on Feb. 6, 2022 to reflect our new
     analytics provider. To ensure your privacy, we left Google Analytics for Fathom Analytics, which
-    does not track you accross multiple websites.</Container
+    does not track you across multiple websites.</Container
   ></note
 >
 
