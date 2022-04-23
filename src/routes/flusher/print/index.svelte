d9326b1742f7d65593909bfb4333361167d22198
Jack Buehner <github@jackbuehner.com>
2022-04-23 03:34:35 +0000

    feat: add print button to flusher print layout

--- src/routes/flusher/print/index.svelte
+++ src/routes/flusher/print/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Button from '$lib/components/Button.svelte';
+
   import { FlusherDoc, type Flusher } from '$lib/components/Flusher';
   import { title } from '$lib/stores/title';
   import { romanize } from 'romans';
@@ -19,5 +21,28 @@
 </script>
 
 {#if flusher}
+  <div class="actions">
+    <Button on:click={() => window.print()}>Print</Button>
+  </div>
   <FlusherDoc {flusher} />
 {/if}
+
+<style>
+  div.actions {
+    background-color: #fafafa;
+    padding: 20px 0 0 0;
+    width: 8.5in;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 6px;
+  }
+
+  @media print {
+    div.actions {
+      display: none;
+    }
+  }
+</style>
