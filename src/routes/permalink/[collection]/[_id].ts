3f05561dec9c615d724c417ed92ac545d0f18c7b
Jack Buehner <github@jackbuehner.com>
2022-04-23 03:26:33 +0000

    feat: add permalink for flusher

--- src/routes/permalink/[collection]/[_id].ts
+++ src/routes/permalink/[collection]/[_id].ts
@@ -1,7 +1,13 @@
-import { GET_ARTICLE_BASIC, type GET_ARTICLE_BASIC__TYPE } from '$lib/queries';
+import {
+  GET_ARTICLE_BASIC,
+  GET_FLUSHERS,
+  type GET_ARTICLE_BASIC__TYPE,
+  type GET_FLUSHERS__TYPE,
+} from '$lib/queries';
 import { api } from '$lib/utils/api';
 import { insertDate } from '$lib/utils/insertDate';
 import type { RequestHandler } from '@sveltejs/kit';
+import { DateTime } from 'luxon';
 
 export const get: RequestHandler<{ collection: string; _id: string }> = async (request) => {
   const { collection, _id } = request.params;
@@ -24,6 +30,22 @@ export const get: RequestHandler<{ collection: string; _id: string }> = async (r
     return {
       status: 302, // temporary redirect
       headers: { Location: `/articles/${year}/${month}/${day}/${slug}` },
+    };
+  }
+
+  if (collection === 'flusher') {
+    // get the article from the api
+    const { data, error } = await api.query<GET_FLUSHERS__TYPE>(GET_FLUSHERS, {
+      variables: { filter: JSON.stringify({ _id }) },
+    });
+
+    // get the article from the response
+    if (error.errors || !data?.flushesPublic?.docs?.[0]) return failureRedirect;
+    const week = DateTime.fromISO(data.flushesPublic.docs[0].timestamps.week).toISODate();
+
+    return {
+      status: 307, // temporary redirect
+      headers: { Location: `/flusher/${week}` },
     };
   }
 
