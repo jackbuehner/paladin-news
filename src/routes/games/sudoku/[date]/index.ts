d1db079fd2570559ed26d6371619f99c9f2d0f55
Jack Buehner <github@jackbuehner.com>
2022-05-11 03:38:35 +0000

    fix: use gmt for date detection

--- src/routes/games/sudoku/[date]/index.ts
+++ src/routes/games/sudoku/[date]/index.ts
@@ -9,12 +9,13 @@ import {
   type GET_SUDOKU__TYPE,
 } from '$lib/queries';
 import { variables } from '$lib/variables';
+import { DateTime } from 'luxon';
 
 export const get: RequestHandler<{ date: string }> = async (request) => {
   let date = request.params.date;
   if (date === 'latest' || date === 'today') date = new Date().toISOString().split('T')[0];
 
-  if (new Date(date) > new Date()) return { status: 418 };
+  if (new Date(date) > new Date(DateTime.now().toISODate())) return { status: 418 };
   if (new Date(date) < new Date('2022-05-01')) return { status: 404 };
 
   // request the puzzle from the api
