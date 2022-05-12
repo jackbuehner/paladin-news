9b27b2f354b182170cb0e7374186445e9cbbee8d
Jack Buehner <github@jackbuehner.com>
2022-05-12 00:23:54 +0000

    feat: add queries for getting crosswords

---
+++ src/queries/GET_CROSSWORD.ts
@@ -0,0 +1,56 @@
+import type { GraphJsonType } from '.';
+
+const GET_CROSSWORD = `
+  query GetCrossword($_id: ObjectID!) {
+    crosswordPublic(_id: $_id) {
+      _id
+      name
+      timestamps {
+        published_at
+        updated_at
+      }
+      words {
+        clue
+        word
+      }
+      people {
+        authors {
+          name
+        }
+        editors {
+          primary {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+interface GET_CROSSWORD__DOC_TYPE {
+  _id: string;
+  name: string;
+  timestamps: {
+    published_at: string;
+    updated_at: string;
+  };
+  words: Array<{
+    word: string;
+    clue: string;
+  }>;
+  people: {
+    authors: Array<{ name: string }>;
+    editors: {
+      primary: Array<{ name: string }>;
+    };
+  };
+}
+
+interface GET_CROSSWORD__TYPE {
+  crosswordPublic?: GET_CROSSWORD__DOC_TYPE;
+}
+
+type GET_CROSSWORD__JSON = GraphJsonType<GET_CROSSWORD__TYPE>;
+
+export { GET_CROSSWORD };
+export type { GET_CROSSWORD__DOC_TYPE, GET_CROSSWORD__TYPE, GET_CROSSWORD__JSON };
