13e5d8d853c9f32c5e23442b2a2c7999a75c3c8e
Jack Buehner <git@jackbuehner.com>
2022-11-13 22:27:20 -0500

    fix: only redirect articles with section title for valid sections from old website

--- src/routes/(standard)/[category]/[slug]/+page.ts
+++ src/routes/(standard)/[category]/[slug]/+page.ts
@@ -1,6 +1,18 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ params }) => {
-  throw redirect(307, `/articles/${params.slug}`);
+export const load: PageLoad = async ({ params, data }) => {
+  // these are the only valid section names from
+  // the url structure that requires this redirect
+  if (
+    params.category === 'news' ||
+    params.category === 'opinions' ||
+    params.category === 'sports' ||
+    params.category === 'arts-culture' ||
+    params.category === 'videos'
+  ) {
+    throw redirect(307, `/articles/${params.slug}`);
+  }
+
+  return data;
 };
