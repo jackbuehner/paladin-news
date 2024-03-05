a83d050d77e10993d1d4637c9a205c5c18edace1
Jack Buehner <git@jackbuehner.com>
2024-03-05 17:33:01 -0500

    feat: spoof date on website to Dec. 31, 2023 at 11:59 PM UTC

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,10 @@
-import type { LayoutLoad } from './$types';
 import { error } from '@sveltejs/kit';
+import MockDate from 'mockdate';
+import type { LayoutLoad } from './$types';
+
+// since this version of the website is retired, we should make
+// all generated dates be the last day the site was the primary one
+MockDate.set('2023-12-31T23:59:00.000Z'); // Dec. 31, 2023 at 11:59 PM UTC
 
 export const load: LayoutLoad = async ({ url }) => {
   // return 204 status when sveltekit tries to load scripts as pages
